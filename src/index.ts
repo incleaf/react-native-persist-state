457c6b9182f726bf185bda920dc0993e29895e37
Hyeonsu Lee <incleaf@gmail.com>
2021-11-10 22:39:22 +0900

    Export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export * from "./types";
 export { createPersistStore } from "./createPersistStore";
 export { usePersistState } from "./usePersistState";
+export {
+  PersistStateProvider,
+  PersistStateClient,
+  PersistStateProviderProps,
+} from "./PersistStateProvider";
